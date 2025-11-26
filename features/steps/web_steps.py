b1a38f45f55eb72ebeeb71f850f97bcaf3b66344
9taails <83721185+9taails@users.noreply.github.com>
2025-11-26 16:19:38 +0100

    Uncomment assertions in web_steps.py

--- features/steps/web_steps.py
+++ features/steps/web_steps.py
@@ -138,9 +138,9 @@ def step_impl(context, message):
 @then('I should see "{name}" in the results')
 def step_impl(context, name):
     element = context.driver.find_element(By.ID, 'search_results')
-    #assert(name in element.text)
+    assert(name in element.text)
 
 @then('I should not see "{name}" in the results')
 def step_impl(context, name):
     element = context.driver.find_element(By.ID, 'search_results')
-    #assert(name not in element.text)
+    assert(name not in element.text)
